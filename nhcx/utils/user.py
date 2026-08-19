56905d81c6993f63cb108719db6976f1963358c2
Khavin Shankar <khavinshankar@gmail.com>
2026-08-19 06:47:01 +0530

    handle human error while auto wallet balance check after claim response

--- nhcx/utils/user.py
+++ nhcx/utils/user.py
@@ -12,11 +12,19 @@ def get_or_create_nhcx_user():
     user, _ = User.objects.get_or_create(
         username=settings.NHCX_USERNAME,
         defaults={
+            "first_name": "NHCX",
+            "last_name": "System",
             "email": "nhcx@ohc.network",
             "phone_number": "917777777777",
             "verified": True,
         },
     )
+
+    if not user.get_full_name().strip():
+        user.first_name = "NHCX"
+        user.last_name = "System"
+        user.save(update_fields=["first_name", "last_name"])
+
     NHCX_USER = user
 
     return user
